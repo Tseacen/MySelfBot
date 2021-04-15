fa2bbbf3fb02cf439920c2d3655a3e466b174e87
Tseacen <ethan.brnt.2.0@gmail.com>
2021-04-15 22:00:31 +0200

    Initial Version

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "token": "NTgzNjkxNzI0NDIxODU3Mjkx.XPADRw.KpLHjpreZ8AmUVMJWh73vWr3emE",
+    "user":"313768730700152833",
+
+    "changeUsername":"false"
+  };
+  
---
+++ index.js
@@ -0,0 +1,84 @@
+const config = require("./config.js");
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+
+
+bot.on("ready", async () => {
+    console.log("Connected")
+    let user = bot.users.cache.get(config.user);
+            
+    bot.user.setAvatar(`${user.displayAvatarURL()}`);
+    if(config.changeUsername === "true") {
+        bot.user.setUsername(user.username)
+    }
+
+    setInterval(
+        async function(){ 
+
+            let user = bot.users.cache.get(config.user);
+            //console.log(user)
+
+            if(!user) return
+
+            
+            //presence
+            //console.log(user)
+            let presence = user.presence.status;
+            if(presence === "online") presence = "online";
+            if(presence === "dnd") presence = "dnd";
+            if(presence === "idle") presence = "idle";
+            if(presence === "offline") presence = "offline";
+
+            let playingStatus = false;
+            let playing = false;
+
+            if(user.presence.activities.length >= 1) {
+                let activity = user.presence.activities.find(x => x.name);
+                if(activity) {
+                    activity = user.presence.activities;
+                    if(activity.find(x => x.type === "CUSTOM_STATUS")) {
+                        let pre = activity.find(x => x.type === "CUSTOM_STATUS");
+                        playingStatus = "WATCHING";
+                        playing = pre.state;
+                    }
+
+                    if(activity.find(x => x.type === "LISTENING")) {
+                        let pre = activity.find(x => x.type === "LISTENING");
+                        playingStatus = "LISTENING";
+                        playing = `${pre.state} - ${pre.details}`;
+                    }
+
+                    if(activity.find(x => x.type === "PLAYING")) {
+                        let pre = activity.find(x => x.type === "PLAYING");
+                        playingStatus = "PLAYING";
+                        playing = pre.name;
+                    }
+
+                };
+            };
+            if(playingStatus && playing) {
+                bot.user.setPresence({ activity: { name: playing, type: playingStatus }, status: presence });
+            } else {
+                bot.user.setStatus(presence)
+            }
+        }
+    , 10000);
+
+
+    setInterval(
+        async function(){ 
+
+            let user = bot.users.cache.get(config.user);
+            
+            bot.user.setAvatar(`${user.displayAvatarURL()}`);
+
+            if(config.changeUsername === "true") {
+                bot.user.setUsername(user.username)
+            }
+        }
+    , 3600000);
+});
+
+
+
+bot.login(config.token);
